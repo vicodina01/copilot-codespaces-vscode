954da21517fb8a1cadf6942279e2ba73c91336af
Vicodina <vicodina01@gmail.com>
2024-03-21 18:50:14 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,9 @@
+function skillMembers(){
+    return [
+        {name: 'John', skill: 'JavaScript'},
+        {name: 'Jane', skill: 'JavaScript'},
+        {name: 'Joe', skill: 'Java'},
+        {name: 'Jack', skill: 'Java'},
+        {name: 'Jill', skill: 'Java'}
+    ];
+}
